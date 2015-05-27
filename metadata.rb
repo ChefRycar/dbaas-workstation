f13ab38638b11de58b7eb8a9810ee029b806071a
Nick Rycar <rycar@chef.io>
2015-05-27 16:54:16 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'dbaas-workstation'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures dbaas-workstation'
+long_description 'Installs/Configures dbaas-workstation'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: dbaas-workstation
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
